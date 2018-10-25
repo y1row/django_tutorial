3884b80cdccf75a21e7eb950d6edbf3084a1c4e4
Yuuichiro Kato <y1row.kato@gmail.com>
2018-10-25 17:22:24 +0900

    Questionテーブル、idをbigintで作っていく

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
