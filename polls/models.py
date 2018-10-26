f3ce334142e145f9c2259c1875ce639d956e8d9f
Yuuichiro Kato <y1row.kato@gmail.com>
2018-10-26 14:06:59 +0900

    回答テーブルだこのやろう

--- polls/models.py
+++ polls/models.py
@@ -5,3 +5,10 @@ class Question(models.Model):
     id = models.BigAutoField(primary_key=True)
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
