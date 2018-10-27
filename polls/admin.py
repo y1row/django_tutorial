b1365cc11a36ebec850c5b444fd184bb36d88f7f
Yuuichiro Kato <y1row.kato@gmail.com>
2018-10-27 16:53:25 +0900

    日本語で表示したいじゃない

--- polls/admin.py
+++ polls/admin.py
@@ -11,7 +11,7 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('公開設定', {'fields': ['pub_date']}),
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
