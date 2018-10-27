92af50d98e9866cf5fe8ee9aeec040714ac6d5e4
Yuuichiro Kato <y1row.kato@gmail.com>
2018-10-27 11:02:43 +0900

    templatesにも名前空間あったほうがいいってよ

--- polls/views.py
+++ polls/views.py
@@ -10,12 +10,12 @@ def index(request):
     context = {
         'latest_question_list': latest_question_list,
     }
-    return render(request, 'index.html', context)
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'detail.html', {'question': question})
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
@@ -28,7 +28,7 @@ def vote(request, question_id):
     try:
         choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'detail.html', {
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': 'その選択肢はありません',
         })
