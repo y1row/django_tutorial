82f2146063ff244e0ff7a2109f0e5a0b620879fc
Yuuichiro Kato <y1row.kato@gmail.com>
2018-10-27 16:43:32 +0900

    モデルにメタデータ付けた

--- polls/models.py
+++ polls/models.py
@@ -6,11 +6,15 @@ from django.utils import timezone
 
 class Question(models.Model):
     id = models.BigAutoField(primary_key=True)
-    question_text = models.CharField(max_length=200, verbose_name='質問')
+    question_text = models.CharField(max_length=200, verbose_name='質問文')
     pub_date = models.DateTimeField('公開日時')
 
     def __str__(self):
         return self.question_text
+
+    class Meta:
+        verbose_name = '質問'
+        verbose_name_plural = '質問'
 
     def was_published_recently(self):
         """ 1日以内に公開された？ """
@@ -25,8 +29,12 @@ class Question(models.Model):
 class Choice(models.Model):
     id = models.BigAutoField(primary_key=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200, verbose_name='回答')
+    choice_text = models.CharField(max_length=200, verbose_name='選択肢')
     votes = models.IntegerField(default=0, verbose_name='得票数')
 
     def __str__(self):
         return self.choice_text
+
+    class Meta:
+        verbose_name = '回答'
+        verbose_name_plural = '回答'
