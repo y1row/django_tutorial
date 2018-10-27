438c6371282f34f282402beb57089757a85199ff
Yuuichiro Kato <y1row.kato@gmail.com>
2018-10-27 16:19:47 +0900

    管理画面の質問、表示項目とかフィルタとか

--- polls/models.py
+++ polls/models.py
@@ -6,8 +6,8 @@ from django.utils import timezone
 
 class Question(models.Model):
     id = models.BigAutoField(primary_key=True)
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question_text = models.CharField(max_length=200, verbose_name='質問')
+    pub_date = models.DateTimeField('公開日時')
 
     def __str__(self):
         return self.question_text
@@ -17,12 +17,16 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = '最近の記事'
+
 
 class Choice(models.Model):
     id = models.BigAutoField(primary_key=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    choice_text = models.CharField(max_length=200, verbose_name='回答')
+    votes = models.IntegerField(default=0, verbose_name='得票数')
 
     def __str__(self):
         return self.choice_text
